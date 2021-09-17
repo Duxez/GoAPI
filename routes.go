f8a03268491adaccc1e79bf0e515aed5cfcd3670
Job Rapati <jrapati@hotmail.nl>
2021-09-17 16:54:43 +0200

    extra test route

--- routes.go
+++ routes.go
@@ -18,8 +18,13 @@ var routes = []Route{
 		routeMethod:   []byte("GET"),
 		routeCallback: test,
 	},
+	{
+		urlRoute:      []byte("/post"),
+		routeMethod:   []byte("POST"),
+		routeCallback: test,
+	},
 }
 
 func test(req *fasthttp.RequestCtx) {
-	fmt.Fprintf(req, "Saying hi through route %q", string(req.Path()))
+	fmt.Fprintf(req, "Saying hi through route %q with method %q", req.Path(), req.Method())
 }
